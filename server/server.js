617035f051dc3e04434cd4ba06b5c7a59f28aa0b
roberto.carlos <hakane_kavuto@hotmail.com>
2020-12-04 14:34:03 -0600

    conexion lista a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
@@ -58,6 +59,19 @@ app.post('/usuario/',function(req,res){
     })
 
   })
+
+  mongoose.connect('mongodb://localhost:27017/cafeteria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }, (err,res) => {
+    if (err) throw err;
+    console.log('Base de datos Online');
+});
+
+
+
 app.listen(process.env.PORT,()=>{
 console.log('el servidor esta corriendo en el puerto 3000')
 
